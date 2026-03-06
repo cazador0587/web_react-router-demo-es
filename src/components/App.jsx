7b7e14a6e7930fda24a391a3f2fddf1515f3f959
Fernando Salvador <fsalvador@ecosistemasmexico.com>
2026-03-05 19:14:59 -0600

    feat - creacion del repositorio y configuracion del proyecto en react

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom"; // Nueva importación
+import Dashboard from "./Dashboard/Dashboard"; // Nueva importación
+import Header from "./Header/Header";
+import Reviews from "./Reviews/Reviews";
+import AboutMe from "./AboutMe/AboutMe";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* Envuelve un componente Route dentro de un componente Routes  
+          y especifica la ruta y los atributos del elemento tal como se muestra. */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
