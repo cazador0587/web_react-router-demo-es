72baa19b9d5a0a665e2636bf24da534ef334c84b
Fernando Salvador <ferydali@gmail.com>
2026-03-09 16:02:40 -0600

    feat - Enrutamamiento Anidado - AboutME

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,7 +1,25 @@
+// AboutMe.jsx
+
+import "./AboutMe.css"; // Nueva importación
+import { Link } from "react-router-dom"; // Nueva importación
+
 function AboutMe() {
+  // Agrega los nombres de clase que se muestran a continuación para que se apliquen los estilos.
+  // Agrega los enlaces como se muestra a continuación.
   return (
-    <div>
-      <p>Soy una persona sencilla. Veo emojis, escribo reseñas.</p>
+    <div className="about">
+      <ul className="links">
+        <li>
+          <Link to="my-story">Mi historia</Link>
+        </li>
+        <li>
+          <Link to="hobbies">Hobbies</Link>
+        </li>
+        <li>
+          <Link to="contact">Mi información de contacto</Link>
+        </li>
+      </ul>
+      <p>Soy una persona sencilla. Veo Emojis, escribo reseñas.</p>
     </div>
   );
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,14 @@ import Header from "./Header/Header";
 import Reviews from "./Reviews/Reviews";
 import AboutMe from "./AboutMe/AboutMe";
 import AboutUs from "./AboutUs/AboutUs";
+// Importa nuevos componentes
+import Contact from './AboutMe/Contact';
+import Hobbies from './AboutMe/Hobbies';
+import MyStory from './AboutMe/MyStory';
 import "./App.css";
 
 function App() {
+  // Agrega las rutas hijo a la ruta /about-me.
   return (
     <div className="App">
       <Header />
@@ -15,7 +20,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
